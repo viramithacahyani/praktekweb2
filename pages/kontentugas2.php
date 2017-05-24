a7ad84fe9d058b65bc3ec2ab906521bc71869091
viramitha <VMCahyani@gmail.com>
2017-05-24 17:01:27 +0700

    update tanggal

--- pages/kontentugas2.php
+++ pages/kontentugas2.php
@@ -12,7 +12,7 @@
                 <p class="lead">
                     by <a href="index.php">Start Bootstrap</a>
                 </p>
-                <p><i class="fa fa-clock-o"></i> Posted on August 28, 2013 at 10:00 PM</p>
+                <p><i class="fa fa-clock-o"></i> Posted on <?php echo date ('d F Y', strtotime($dt['tanggal'])); ?></p>
                 <hr>
                 
                 <hr>
